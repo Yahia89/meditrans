1235093d8accaf173967c6c3c131a0440e1e5b0b
Yahia Alhejoj <yahiaalhejoj@yahoo.com>
2026-09-26 09:35:08 -0500

    Updates

--- assets/skeleton-BQR3vLfq.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./vendor-query-IVBxe8cG.js";import{c as s}from"./index-NdVKbe2w.js";function r({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
